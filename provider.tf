1b9cfa3039e2f483e8e90b8be9b0a2331dd35be4
unknown <sinha.shambhu@gmail.com>
2025-08-30 17:54:15 +0530

    backend block add

--- provider.tf
+++ provider.tf
@@ -5,9 +5,16 @@ terraform {
       version = "4.42.0"
     }
   }
+  backend "azurerm" {
+  resource_group_name  = "terra-sam"
+  storage_account_name = "stgshambhu101"
+  container_name       = "samcont"
+  key                  = "terraform.tfstate"
+}
 }
 
 provider "azurerm" {
     features {}
     subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
 }
+
